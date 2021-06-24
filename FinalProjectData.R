239d352c5964cd496ea00d03c185eb3404e9aa49
GabeYou <gyouos76@gmail.com>
2021-06-23 20:38:00 -0400

    new commit

--- FinalProjectData.R
+++ FinalProjectData.R
@@ -1,11 +1,19 @@
-raw_data <- read_html("https://sinoalice.game-db.tw/weapons")
+library(tidyverse)
+library(rvest)
+raw_data<- read_html("https://sinoalice.game-db.tw/weapons")
 x<- raw_data %>% 
-html_elements(".colMaxTotal") %>% 
-  html_text2() %>% 
+html_elements("td:nth-child(10)") %>% 
+ html_text2() %>% 
   as_tibble() 
-
+x
+y<- raw_data %>% 
 html_elements(".colCost") %>% 
   html_text2() %>% 
   as_tibble() 
 
 write_csv2(x, "FinalProjectData.csv")
+write_csv2(y, "FinalProjectDataY.csv")
+
+
+
+
